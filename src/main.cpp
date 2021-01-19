1b09caa713e822440b38666bbf6f5b2a0bb11dff
Günter Schmitt <macuser59@gmail.com>
2021-01-19 17:06:49 +0100

    new colors and centered

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,45 @@
  Gilchrist 6/2/2014 1.0
  Updated by Bodmer
  */
+/***************************************************************************************
+**                         Section 6: Colour enumeration
+***************************************************************************************/
+// Default color definitions
+#define TFT_BLACK       0x0000      /*   0,   0,   0 */
+#define TFT_NAVY        0x000F      /*   0,   0, 128 */
+#define TFT_DARKGREEN   0x03E0      /*   0, 128,   0 */
+#define TFT_DARKCYAN    0x03EF      /*   0, 128, 128 */
+#define TFT_MAROON      0x7800      /* 128,   0,   0 */
+#define TFT_PURPLE      0x780F      /* 128,   0, 128 */
+#define TFT_OLIVE       0x7BE0      /* 128, 128,   0 */
+#define TFT_LIGHTGREY   0xD69A      /* 211, 211, 211 */
+#define TFT_DARKGREY    0x7BEF      /* 128, 128, 128 */
+#define TFT_BLUE        0x001F      /*   0,   0, 255 */
+#define TFT_GREEN       0x07E0      /*   0, 255,   0 */
+#define TFT_CYAN        0x07FF      /*   0, 255, 255 */
+#define TFT_RED         0xF800      /* 255,   0,   0 */
+#define TFT_MAGENTA     0xF81F      /* 255,   0, 255 */
+#define TFT_YELLOW      0xFFE0      /* 255, 255,   0 */
+#define TFT_WHITE       0xFFFF      /* 255, 255, 255 */
+#define TFT_ORANGE      0xFDA0      /* 255, 180,   0 */
+#define TFT_GREENYELLOW 0xB7E0      /* 180, 255,   0 */
+#define TFT_PINK        0xFE19      /* 255, 192, 203 */ //Lighter pink, was 0xFC9F      
+#define TFT_BROWN       0x9A60      /* 150,  75,   0 */
+#define TFT_GOLD        0xFEA0      /* 255, 215,   0 */
+#define TFT_SILVER      0xC618      /* 192, 192, 192 */
+#define TFT_SKYBLUE     0x867D      /* 135, 206, 235 */
+#define TFT_VIOLET      0x915C      /* 180,  46, 226 */
+
+
+
+
+
 
 #include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
 #include <SPI.h>
 
 TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 
-#define TFT_GREY 0xBDF7
 
 float sx = 0, sy = 1, mx = 1, my = 0, hx = -1, hy = 0;    // Saved H, M, S x & y multipliers
 float sdeg=0, mdeg=0, hdeg=0;
@@ -38,30 +70,32 @@ boolean initial = 1;
 void setup(void) {
   tft.init();
   tft.setRotation(0);
-  tft.fillScreen(TFT_GREY);
-  tft.setTextColor(TFT_GREEN, TFT_GREY);  // Adding a black background colour erases previous text automatically
+  tft.fillScreen(TFT_BROWN);
+  tft.setTextColor(TFT_GREEN, TFT_BROWN);  // Adding a black background colour erases previous text automatically
   
   // Draw clock face
-  tft.fillCircle(64, 64, 61, TFT_BLUE);
-  tft.fillCircle(64, 64, 57, TFT_BLACK);
+  tft.fillCircle(66, 64, 61, TFT_BLUE);
+  tft.fillCircle(66, 64, 57, TFT_DARKGREY);
+  //delay(2000);
 
   // Draw 12 lines
   for(int i = 0; i<360; i+= 30) {
     sx = cos((i-90)*0.0174532925);
     sy = sin((i-90)*0.0174532925);
-    x0 = sx*57+64;
+    x0 = sx*57+66;
     yy0 = sy*57+64;
-    x1 = sx*50+64;
+    x1 = sx*50+66;
     yy1 = sy*50+64;
 
     tft.drawLine(x0, yy0, x1, yy1, TFT_BLUE);
   }
+    //delay(2000);
 
   // Draw 60 dots
   for(int i = 0; i<360; i+= 6) {
     sx = cos((i-90)*0.0174532925);
     sy = sin((i-90)*0.0174532925);
-    x0 = sx*53+64;
+    x0 = sx*53+66;
     yy0 = sy*53+64;
     
     tft.drawPixel(x0, yy0, TFT_BLUE);
@@ -70,13 +104,15 @@ void setup(void) {
     if(i==90 || i==270) tft.fillCircle(x0, yy0, 1, TFT_CYAN);
     if(i==90 || i==270) tft.fillCircle(x0+1, yy0, 1, TFT_CYAN);
   }
+    //delay(2000);
 
-  tft.fillCircle(65, 65, 3, TFT_RED);
-
+  //tft.fillCircle(67, 65, 3, TFT_RED);
+ 
+  
   // Draw text at position 64,125 using fonts 4
   // Only font numbers 2,4,6,7 are valid. Font 6 only contains characters [space] 0 1 2 3 4 5 6 7 8 9 : . a p m
   // Font 7 is a 7 segment font and only contains characters [space] 0 1 2 3 4 5 6 7 8 9 : .
-  tft.drawCentreString("Time flies",64,130,4);
+  tft.drawCentreString("Time flies",66,130,4);
 
   targetTime = millis() + 1000; 
 }
@@ -111,23 +147,23 @@ void loop() {
     if (ss==0 || initial) {
       initial = 0;
       // Erase hour and minute hand positions every minute
-      tft.drawLine(ohx, ohy, 65, 65, TFT_BLACK);
-      ohx = hx*33+65;    
+      tft.drawLine(ohx, ohy, 67, 65, TFT_DARKGREY);
+      ohx = hx*33+67;    
       ohy = hy*33+65;
-      tft.drawLine(omx, omy, 65, 65, TFT_BLACK);
-      omx = mx*44+65;    
+      tft.drawLine(omx, omy, 67, 65, TFT_DARKGREY);
+      omx = mx*44+67;    
       omy = my*44+65;
     }
 
       // Redraw new hand positions, hour and minute hands not erased here to avoid flicker
-      tft.drawLine(osx, osy, 65, 65, TFT_BLACK);
-      tft.drawLine(ohx, ohy, 65, 65, TFT_WHITE);
-      tft.drawLine(omx, omy, 65, 65, TFT_WHITE);
-      osx = sx*47+65;    
+      tft.drawLine(osx, osy, 67, 65, TFT_DARKGREY);
+      tft.drawLine(ohx, ohy, 67, 65, TFT_WHITE);
+      tft.drawLine(omx, omy, 67, 65, TFT_WHITE);
+      osx = sx*47+67;    
       osy = sy*47+65;
-      tft.drawLine(osx, osy, 65, 65, TFT_RED);
+      tft.drawLine(osx, osy, 67, 65, TFT_RED);
 
-    tft.fillCircle(65, 65, 3, TFT_RED);
+    tft.fillCircle(67, 65, 3, TFT_RED);
   }
 }
 
